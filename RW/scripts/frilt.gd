37a30108d8097d1043a6bbe8f18c124c0d548384
Matteo <matteomccaughey@gmail.com>
2025-04-28 21:29:23 +0100

    Modified movement to make the bot follow the mouses relative angle to origin

--- RW/scripts/frilt.gd
+++ RW/scripts/frilt.gd
@@ -1,43 +1,65 @@
 extends CharacterBody2D
 
 const SPEED = 100
-
+var aim_angle = 0.0
+var mouse_lock_radius = 300
 # Weapons
 @export var equipped_weapons: Array[Weapon] = [null, null,null]  #Left, Right
 
 @onready var weapon_sprites = {
 	"left": $LeftArm,
 	"right": $RightArm,
-	"thirdLeg": $test
 }
 
 func _ready():
 	# Example of equipping weapons
 	var Gun = preload("res://weapons/Gun.tres")
 	var Knife = preload("res://weapons/Knife.tres")
+	
+	#Hide mouse on start
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+	
 
 	equip_weapon(0, Gun) 
 	equip_weapon(1, Knife) 
-	equip_weapon(2, Knife) 
 
 
 func _physics_process(delta):
 	player_movement(delta)
+	
+func _process(delta):
+	update_aim_direction()
+	var centre = get_viewport_rect().size / 2
+	var mouse_pos = get_viewport().get_mouse_position() / 2
+	
+	var to_mouse = mouse_pos - centre
+	
+	
 
 func player_movement(delta):
-	var mouse_pos = get_global_mouse_position()
-	var direction = (mouse_pos - global_position).normalized()
-
-	if global_position.distance_to(mouse_pos) > 5:
-		velocity = direction * SPEED
+	var input_vector = Input.get_vector("move_backward", "move_forward", "move_left", "move_right")
+	
+	if input_vector.length() > 0:
+		input_vector = input_vector.normalized()
+		var move_direction = input_vector.rotated(aim_angle)
+		velocity = move_direction * SPEED
 		move_and_slide()
-
-		rotation = direction.angle() + PI/2
-
-		play_body_anim(1)  # moving
+		play_body_anim(1)
 	else:
 		velocity = Vector2.ZERO
-		play_body_anim(0)  # idle
+		play_body_anim(0)
+	
+	
+	rotation = aim_angle + PI/2
+	
+func update_aim_direction():
+	var viewport_center = get_viewport_rect().size / 2
+	var mouse_on_screen = get_viewport().get_mouse_position()
+	var to_mouse = mouse_on_screen - viewport_center
+	aim_angle = to_mouse.angle()
+
+
 
 func play_body_anim(movement):
 	var anim = $AnimatedSprite2D
@@ -45,12 +67,10 @@ func play_body_anim(movement):
 	if movement == 1:
 		weapon_sprites["left"].play("move")
 		weapon_sprites["right"].play("move")
-		weapon_sprites["thirdLeg"].play("move")
 		anim.play("move")
 	else:
 		weapon_sprites["left"].play("idle")
 		weapon_sprites["right"].play("idle")
-		weapon_sprites["thirdLeg"].play("idle")
 		anim.play("idle")
 
 func equip_weapon(slot_index: int, weapon: Weapon):
@@ -60,7 +80,6 @@ func equip_weapon(slot_index: int, weapon: Weapon):
 		match slot_index:
 			0: setup_weapon_sprite(weapon_sprites["left"], weapon)
 			1: setup_weapon_sprite(weapon_sprites["right"], weapon)
-			2: setup_weapon_sprite(weapon_sprites["thirdLeg"], weapon)
 
 	else:
 		print("Invalid slot index: ", slot_index)
